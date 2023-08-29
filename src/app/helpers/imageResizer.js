3cc9848c9e46bd40f7bb867fec9e46629cadd9e4
Mehedi Hasan <76158620+Mehedi-MHS@users.noreply.github.com>
2023-08-29 12:33:00 +0600

    Update imageResizer.js

--- src/app/helpers/imageResizer.js
+++ src/app/helpers/imageResizer.js
@@ -43,40 +43,7 @@ async function imageResizer(file, resizeValue) {
     info.resizedData = resizedDataURL;
     return info;
 
-    /*
-  
-  reader.onload = async (event) => {
-    const img = new Image();
-    img.src = event.target.result;
-
-    // in raw js, we have to use a promise to use await.
-    await new Promise((resolve) => {
-      img.onload = () => {
-        info.realWidth = img.width;
-        info.realHeight = img.height;
-        info.resizedWidth = info.realWidth * (1 - resizeValue); //resizeValue is between 0-1
-        info.resizedHeight = info.realHeight * (1 - resizeValue);
-        resolve(info);
-      };
-    });
-
-    //Creating offscreen (invisible) canvas for resizing image
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = info.resizedWidth;
-    canvas.height = info.resizedHeight;
-
-    // 'file' provided as parameter is an object. We cannot draw it directly in to the canvas
-    // To draw it, first create an image;
-    ctx.drawImage(img, 0, 0, info.resizedWidth, info.resizedHeight);
-    const resizedImageData = canvas.toDataURL(file.type);
-    info.resizedSize = resizedImageData.length;
-    info.resizedData = resizedImageData;
-  }; //reader.onload
-  reader.readAsDataURL(file);
-  return info;
-
-*/
+    
   } catch (error) {
     console.error("Error in imageResizer:", error);
     throw error;
